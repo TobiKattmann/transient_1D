Bugfix for Adjoint calculation, correct equation + objective func

src/transient_1D.py
@@ -121,9 +121,10 @@ class Transient_1D_Diffusion:
     """docstring TODO"""
     obj = 0.
     for n in range(self.Nt):
-      for i in range(len(self.alpha)):
-        obj += self.mesh.dx * self.alpha[i] * self.full_solution[n][i] 
-      obj *= self.dt
+      if n == self.Nt-1:#HERE
+        for i in range(len(self.alpha)):
+          obj += self.mesh.dx * self.alpha[i] * self.full_solution[n][i] 
+        obj *= self.dt
 
     self.obj = obj
     return obj
@@ -132,6 +133,7 @@ class Transient_1D_Diffusion:
     """docstring TODO"""
     self.djdu = self.alpha
     self.u_init = np.zeros(self.mesh.numnodes)
+    self.u_init = -self.alpha#HERE
     adjoint_solution, self.adjointRes = Numerics.perform_dualTimeStepping(self.Nt, self.dt, self.dt, self.NPt, self.u_init, self.u_left, self.u_right, self.get_AdjointResidual, reverse_timestepping_factor=-1.)
     self.adjoint_solution = list(reversed(adjoint_solution))
 
@@ -143,7 +145,7 @@ class Transient_1D_Diffusion:
       self.A1 = TMP[:, np.newaxis]*self.FD1.toarray()
       self.A2 = self.D[:, np.newaxis]*self.FD2.toarray()
       self.FirstTime_AdjointResidual = False
-    R =  -(- self.A1.T - self.A2.T)@l - self.djdu.T
+    R =  -(- self.A1.T - self.A2.T)@l #- self.djdu.T HERE
     return R
 
   def calculateSensitivities(self):
@@ -243,7 +245,7 @@ if __name__ == '__main__':
     VisuNew.Sensitivities(Sensitivities, show=False, save=True)
     VisuNew.DiffusionCoefficient(DiffusionCoefficients, show=False, save=True)
 
-  if False:
+  if True:
     #cleanup old files
     os.system('rm -v ../NEWimages-tobi-code/*')
 
@@ -256,7 +258,7 @@ if __name__ == '__main__':
       init = np.array([math.exp(-(x-15)**2) for x in getattr(mesh,'X')])
       alpha = np.array([math.exp(-(x-20)**2) for x in getattr(mesh,'X')]) # for objective function
       #sim = TransHeatEq1D(filename, mesh, init, alpha, D, T0=0, T1=0, Nt=4000)
-      sim = Transient_1D_Diffusion(mesh, D, init, 0, 0 , Nt=8000, alpha=alpha)
+      sim = Transient_1D_Diffusion(mesh, D, init, 0, 0 , Nt=4000, alpha=alpha)
       # Solver
       sim.calculatePrimal()
       J = sim.calculateObjectiveFunction()
@@ -271,7 +273,7 @@ if __name__ == '__main__':
     derivative_handle = lambda sim: derivative(sim)
     initial_DesignVars = np.ones(num_nodes)*2.
 
-    opt = optimizer.Optimizer(obj_handle, derivative_handle, initial_DesignVars, num_DesignCycles=5, factorDesignVarUpdate=10)
+    opt = optimizer.Optimizer(obj_handle, derivative_handle, initial_DesignVars, num_DesignCycles=15, factorDesignVarUpdate=10)
     opt.optimize()
 
   if False:
@@ -303,8 +305,9 @@ if __name__ == '__main__':
     plt.show()
 
     FD_sens.Plot_Sensitivities(mesh.X)
+    FDderivative = copy.copy(dJda)
 
-  if True: # with Numerics2
+  if False: # with Numerics2
     num_nodes = 101
     Nt = 4000
     mesh = mesh_1D.mesh1D(begin=0, end=35, numnodes=num_nodes)
@@ -319,17 +322,39 @@ if __name__ == '__main__':
     sim.calculateAdjoint()
     t2=time.time()
     sim.calculateSensitivities()
+    AdjointDerivative = copy.copy(sim.Dderivative)
     t3=time.time()
     print(t1-t0,t2-t1,t3-t2)
 
     VisuNew = visualization.Visualization(sim)
     VisuNew.Residuals(save=True,show=False)
-    VisuNew.Animate_Primal(show=False, save=False)
-    VisuNew.Animate_Adjoint(show=False, save=False)
-    VisuNew.Animate_Primal_and_Adjoint(show=False, save=False)
+    VisuNew.Animate_Primal(show=True, save=False)
+    VisuNew.Animate_Adjoint(show=True, save=False)
+    VisuNew.Animate_Primal_and_Adjoint(show=True, save=False)
     VisuNew.PrimalAdjointSensitivitiesDiffusivity(show=False, save=True)
     #VisuNew.ObjectiveFunction(False, True, cycles, Objective_Function)
     VisuNew.Sensitivities([sim.Dderivative], show=False, save=True)
     #VisuNew.DiffusionCoefficient(DiffusionCoefficients, show=False, save=True)
     a =1
+
+  compareFDandAdjoint = False
+  if compareFDandAdjoint == True:
+    visualization.compareFDandAdjointSensitivities(sim.mesh.X, AdjointDerivative, FDderivative)
+    
+  adjoint_check = False
+  if adjoint_check == True:
+    print(sim.dt, sim.Nt, D)
+    eta = np.sqrt((1. + 4*D*sim.dt*sim.Nt)/1.)
+    analytical_solution = -np.exp(-((np.array(sim.mesh.X)-20.)**2)/eta**2)/eta
+
+    plt.plot(sim.mesh.X, analytical_solution, label="Analytical")
+    plt.plot(sim.mesh.X, sim.adjoint_solution[0], marker=".",label="Numerical")
+  
+    plt.title("Adjoint solution")
+    plt.xlabel("x")
+    plt.ylabel("i")
+    plt.grid(True, axis='both')
+    plt.legend()
+    plt.savefig('adjoint_sol', bbox_inches='tight')
+    plt.show()
 

src/transient_1D_unittests.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 import transient_1D as solver
 import mesh_1D
@@ -75,17 +76,27 @@ class Tests_Adjoint(unittest.TestCase):
   def test_FundamentalSolution(self): 
     """All zero Test. BC, INIT is zero, solution is all zero at all times."""
     filename = 'UNITTEST_ZeroBCandINIT'
-    num_nodes = 11
-    mesh = mesh_1D.mesh1D(begin=0, end=1, numnodes=num_nodes)
+    num_nodes = 101
+    mesh = mesh_1D.mesh1D(begin=0, end=35, numnodes=num_nodes)
     init = np.array([math.exp(-(x-15)**2) for x in getattr(mesh,'X')])
     alpha = np.array([math.exp(-(x-20)**2) for x in getattr(mesh,'X')]) # for objective function
     D = np.ones(num_nodes)
-    sim = solver.Transient_1D_Diffusion(mesh, D, init, 0, 0, Nt=2, alpha=alpha)
+    sim = solver.Transient_1D_Diffusion(mesh, D, init, 0, 0, Nt=4000, alpha=alpha)
     sim.calculatePrimal()
     sim.calculateAdjoint()
 
     eta = np.sqrt((1. + 4*D*sim.dt*sim.Nt)/1.)
     analytical_solution = -np.exp(-((np.array(sim.mesh.X)-20.)**2)/eta**2)/eta
+
+    #plt.close("all")
+    #plt.plot(sim.mesh.X, analytical_solution, label="ana")
+    #plt.plot(sim.mesh.X, sim.adjoint_solution[0], marker=".", label="num")
+    #plt.legend()
+    #plt.axis([0,35,1,-1])
+    #plt.savefig('adjoint_sol_unittest', bbox_inches='tight')
+    #plt.show()
+  
+
     self.assertTrue(np.allclose(sim.adjoint_solution[0], analytical_solution, rtol=1e-3, atol=1e-2))
 
 class Tests_Sensitivities(unittest.TestCase):

src/visualization.py
@@ -105,18 +105,19 @@ class Visualization:
       return lines
 
     def update_line_primal(i):
+      i *=4
       xlist = [self.sim.mesh.X, self.sim.mesh.X]
-      ylist = [self.sim.full_solution[i], self.sim.adjoint_solution[i]]
+      ylist = [self.sim.full_solution[i], self.sim.adjoint_solution[-i]]
 
       for lnum, line in enumerate(lines):
         line.set_data(xlist[lnum], ylist[lnum])
       return lines
 
-    sol_ani = animation.FuncAnimation(fig, update_line_primal, init_func=init, frames=self.sim.Nt, blit=True, interval=1)
+    sol_ani = animation.FuncAnimation(fig, update_line_primal, init_func=init, frames=int(self.sim.Nt/4), blit=True, interval=0.01)
     plt.plot(self.sim.mesh.X,self.sim.full_solution[1], label='initial primal', c='black')
     plt.plot(self.sim.mesh.X,self.sim.full_solution[-1], label='final primal', c='grey')
-    plt.plot(self.sim.mesh.X,self.sim.adjoint_solution[1], label='initial adjoint', c='black')
-    plt.plot(self.sim.mesh.X,self.sim.adjoint_solution[-1], label='final adjoint', c='grey')
+    plt.plot(self.sim.mesh.X,self.sim.adjoint_solution[1], label='final adjoint', c='black')
+    plt.plot(self.sim.mesh.X,self.sim.adjoint_solution[-1], label='initial adjoint', c='grey')
     
     ax.set_title('Primal and Adjoint solution')
     ax.set_xlabel('solution')
@@ -197,13 +198,32 @@ class Visualization:
 
   def template(self, show=False, save=False):
     """Docstring."""
-    ...  
   
     fullImagePath = ''.join([self.folderpath, "image_name.png"])
     if save: plt.savefig(fullImagePath, bbox_inches='tight')
     if show: plt.show()
     plt.close("all")
 
+#---------------------------------------------------------------------------------------#
+def compareFDandAdjointSensitivities(mesh, AdjointDerivative, FDDerivative, show=True, save=True):
+  """Docstring."""
+  folderpath = "./images/"
+
+  plt.plot(mesh, AdjointDerivative/np.amax(np.absolute(AdjointDerivative)), label="Adjoint Sens")
+  plt.plot(mesh, FDDerivative/np.amax(np.absolute(FDDerivative)), label="FD Sens")
+
+  plt.title("normed Sensitivities")
+  plt.xlabel("x")
+  plt.grid(True, axis='both')
+  plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+  plt.legend()
+
+  fullImagePath = ''.join([folderpath, "FDandAdjointSens.png"])
+  if save: plt.savefig(fullImagePath, bbox_inches='tight')
+  if show: plt.show()
+  plt.close("all")
+
+#---------------------------------------------------------------------------------------#
 class DEBUG_Visualization:
   """Provides visualization for debugging purposes."""
   def DEBUGGING_plot_adjoint(self, adj, save_name):
@@ -265,4 +285,3 @@ class DEBUG_Visualization:
 
 if __name__ == '__main__':
   print("Nothing to excecute here. But there is no error either, that's fine.")
-